a9b8cbc5c31dbc7ea4757ada482b8003b19ceb7c
Abe <jaderizan@gmail.com>
2022-02-07 20:35:48 +0700

    up

---
+++ src/TipeDataNumber.java
@@ -0,0 +1,12 @@
+public class TipeDataNumber {
+    public static void main(String[] args) {
+
+        byte inibyte = 127;
+        short inishort = 1000;
+        int iniInt = 1000000000;
+        long iniLong = 1111111111;
+        long inilong = 1111111111L;
+
+
+    }
+}
