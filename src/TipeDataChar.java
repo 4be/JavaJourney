06457a35ce9029ff2f9dcbb1d12b52ec949bfe17
Abe <jaderizan@gmail.com>
2022-02-09 23:55:47 +0700

    tipe data number

---
+++ src/TipeDataChar.java
@@ -0,0 +1,15 @@
+public class TipeDataChar {
+
+    public static void main(String[] args) {
+
+        char a = 'A';
+        char b = 'B';
+        char e = 'E';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(e);
+
+    }
+
+}
